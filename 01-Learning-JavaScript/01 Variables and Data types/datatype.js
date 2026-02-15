f5949385681de25c50d64b9adabe8993d33d8394
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2026-02-15 14:56:42 +0530

    Update datatype.js

--- 01-Learning-JavaScript/01 Variables and Data types/datatype.js
+++ 01-Learning-JavaScript/01 Variables and Data types/datatype.js
@@ -1,54 +1,53 @@
-let a = 7;
+"use strict";
 
-let str = "umesh"
+// 1️⃣ Checking Data Types
+const numberValue = 7;
+const name = "umesh";
+const isActive = false;
+let notAssigned;
 
-let bool = false
+console.log(
+  typeof numberValue,
+  typeof name,
+  typeof isActive,
+  typeof notAssigned
+);
 
-let b;
+// 2️⃣ Reverse a Number (Function Version)
+function reverseNumber(number) {
+  let reversed = 0;
 
-
-console.log(typeof(a),typeof(str),typeof(bool),typeof(b))
-
-let num = 616;
-
-let original = num;
-
-let temp = 0;
-
-while(num > 0){
-
-    let rem = num % 10;
-
-    temp = rem + (temp*10)
-
-    num = Math.floor(num/10)
-
-}
-
-let arr = [1, 3, 4, 6, 7, 8];
-let found = false;
-
-for (let num1 of arr) {
-  if (num1 === 6) {
-    console.log("Good");
-    found = true;
-    break;
+  while (number > 0) {
+    const remainder = number % 10;
+    reversed = remainder + reversed * 10;
+    number = Math.floor(number / 10);
   }
+
+  return reversed;
 }
 
-if (!found) {
+const originalNumber = 616;
+const reversedNumber = reverseNumber(originalNumber);
+
+console.log("Original:", originalNumber);
+console.log("Reversed:", reversedNumber);
+
+// 3️⃣ Searching in Array (Modern Way)
+const numbersArray = [1, 3, 4, 6, 7, 8];
+
+if (numbersArray.includes(6)) {
+  console.log("Good");
+} else {
   console.log("Not good");
 }
 
-
-
+// 4️⃣ Block Scope Example
 {
-    const  b = 99;
-    console.log(b)
+  const blockValue = 99;
+  console.log("Block Value:", blockValue);
 }
 
-
-let big = 8888888.33333;
-console.log(typeof(big))
-
+// 5️⃣ Floating Number Type
+const bigNumber = 8888888.33333;
+console.log(typeof bigNumber);
 

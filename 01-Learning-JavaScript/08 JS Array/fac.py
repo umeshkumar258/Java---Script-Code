bd23f0f708be020a3bd34ce9f984b3768e9dda21
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2026-02-21 09:52:03 +0530

    Refactor factorial calculation and add input validation
    Refactored factorial calculation into a function and improved input handling.

--- 01-Learning-JavaScript/08 JS Array/fac.py
+++ 01-Learning-JavaScript/08 JS Array/fac.py
@@ -1,11 +1,18 @@
-num = int(input("Enter the number: "))
-
-fac = 1
-
-
-for index in range(1,num+1):
+def factorial(n):
+    """Return factorial of a number."""
+    if n < 0:
+        return "Factorial is not defined for negative numbers."
     
-    fac = index * (fac)
+    result = 1
+    for i in range(1, n + 1):
+        result *= i   # cleaner multiplication
     
-    
-print(fac)
+    return result
+
+
+# Taking user input
+try:
+    num = int(input("Enter a number: "))
+    print("Factorial:", factorial(num))
+except ValueError:
+    print("Please enter a valid integer.")

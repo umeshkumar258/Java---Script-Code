a9d579a16819a716e9265727395a6836d05fb2f1
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2026-04-23 21:54:26 +0530

    Enhance string manipulation examples in tempCodeRunnerFile.js
    Updated console logs to include dynamic character access and added length check.

--- 01-Learning-JavaScript/07 JS/tempCodeRunnerFile.js
+++ 01-Learning-JavaScript/07 JS/tempCodeRunnerFile.js
@@ -1,6 +1,13 @@
-console.log("Umesh is back");
+console.log("Umesh is back 🚀");
 
-let str = "Umesh";
+const str = "Umesh";
 
-console.log(str[0]); // U
-console.log(str[4]); // h
+// Access characters using index (0-based)
+console.log("First character:", str[0]);   // U
+console.log("Last character:", str[4]);    // h
+
+// Better way to get last character (dynamic)
+console.log("Last character (recommended):", str[str.length - 1]);
+
+// Extra: check length
+console.log("Length of string:", str.length);

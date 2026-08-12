d0d51e0034381156f7220cd953f63a99b4781b89
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2026-08-12 22:07:09 +0530

    Add basic arithmetic operations in var.js

---
+++ 01-Learning-JavaScript/01 Variables and Data types/var.js
@@ -0,0 +1,12 @@
+let a = 10;
+let b = 20;
+
+let sum = a + b;
+let difference = a - b;
+let product = a * b;
+let division = a / b;
+
+console.log(sum);
+console.log(difference);
+console.log(product);
+console.log(division);

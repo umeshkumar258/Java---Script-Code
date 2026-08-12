aed42664a6c1c5db580978db58e36b2a478bf0c1
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2026-08-12 22:08:34 +0530

    Add datatypes.js to demonstrate JavaScript types

---
+++ 01-Learning-JavaScript/02 JS/datetypes.js
@@ -0,0 +1,6 @@
+console.log(typeof "Hello");     // string
+console.log(typeof 25);          // number
+console.log(typeof true);        // boolean
+console.log(typeof undefined);   // undefined
+console.log(typeof 123n);        // bigint
+console.log(typeof {});          // object

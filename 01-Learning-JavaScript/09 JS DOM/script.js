8400f40bcd53b9ac103a616ab2188beef1c6b0b0
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2026-08-16 10:07:15 +0530

    Add script.js to manipulate DOM on load

---
+++ 01-Learning-JavaScript/09 JS DOM/script.js
@@ -0,0 +1,14 @@
+// Run JS after DOM loads
+document.addEventListener("DOMContentLoaded", () => {
+
+    // Change page title
+    document.title = "Umesh is Good";
+    console.log("Title:", document.title);
+
+    // Log body
+    console.log("Body:", document.body);
+
+    // Change background color
+    document.body.style.backgroundColor = "red";
+
+});

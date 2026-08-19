8c450b1a45aee114370ce98a6cd43a3eda4ce7b7
Umesh Kumar JB <umeshkumarjb143@gmail.com>
2026-08-19 11:43:47 +0530

    Add string access examples in str.js

---
+++ 01-Learning-JavaScript/07 JS/str.js
@@ -0,0 +1,22 @@
+console.log("String Accessing 🚀");
+
+const str = "Umesh";
+
+// Accessing characters using index
+console.log("First character:", str[0]);
+console.log("Second character:", str[1]);
+console.log("Third character:", str[2]);
+console.log("Fourth character:", str[3]);
+console.log("Fifth character:", str[4]);
+
+// Accessing the last character dynamically
+console.log("Last character:", str[str.length - 1]);
+
+// Checking the length
+console.log("Length:", str.length);
+
+// Using charAt()
+console.log("Character at index 2:", str.charAt(2));
+
+// Invalid index
+console.log("Invalid index:", str[10]);
